322842d298b573b8c794d1c34258a61753ef8f8e
Larissa Santos <larissa21072000@gmail.com>
2025-06-09 00:22:07 -0300

    Finalizado

--- 1º Fase 2020/musica_para_todos.py
+++ 1º Fase 2020/musica_para_todos.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 def musica_para_todos(n, m, c, preferencias):
     # Mapear música detestada para lista de amigos que a detestam
     detestam = {}
@@ -36,40 +35,3 @@ preferencias = [tuple(map(int, input().split())) for _ in range(n)]
 
 
 print(musica_para_todos(n, m, c, preferencias))
-=======
-def musica_para_todos():
-    import sys
-    input = sys.stdin.readline
-
-    N, M, C = map(int, input().split())
-    amigos = [tuple(map(int, input().split())) for _ in range(N)]
-
-    # Para cada música, lista dos amigos que detestam essa música (índices)
-    detestam = [[] for _ in range(M+1)]
-
-    for i, (A, D) in enumerate(amigos):
-        detestam[D].append(i)
-
-    musica_atual = C
-    trocas = 0
-    usadas = set([musica_atual])  # para detectar ciclos
-
-    while True:
-        if not detestam[musica_atual]:
-            # Nenhum amigo detesta essa música, todos satisfeitos
-            print(trocas)
-            return
-        
-        # Amigo que detesta a música e é cliente mais antigo
-        amigo_indice = detestam[musica_atual][0]
-        musica_nova = amigos[amigo_indice][0]  # música que ele adora
-
-        if musica_nova in usadas:
-            # Ciclo infinito detectado
-            print(-1)
-            return
-
-        musica_atual = musica_nova
-        trocas += 1
-        usadas.add(musica_atual)
->>>>>>> cc68966efa5a0b5d1fe8175c84c37ff1166fd5b2
